759d04913b9558977e1eed7922f028db030854cb
andre.freddi <52337680+andrefreddi@users.noreply.github.com>
2020-05-05 22:06:57 -0300

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+	
+	printf("Pi po po po aqui tem uma lisa duplamente encadeada ok?\n");
+	printf("~~ Tenho nv 100 em ilusao e vc nem percebe que aqui nao tem nada");
+	
+	return 0;
+}
